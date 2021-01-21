ee452cb0a37bcc9387726419ff4bc821e7c2c4e5
unknown <ajalilov3@gmail.com>
2021-01-21 14:40:31 -0800

    updated my_mode

---
+++ lambdata_azico2/helper_functions.py
@@ -0,0 +1,40 @@
+def null_count(df):
+    return df.isnull().sum().sum()
+
+# Split addresses into three columns (df['city'], df['state'], and df['zip'])
+# def addy_split(add_series):
+#     df[len_str] = df['owner city state zip'].str.split(',').apply(len)
+#     df['num_commas'] = df['Owner City State Zip'].str.count(',')
+#     return df
+
+
+# Return a new column with the full name from
+# a State abbreviation column -> An input of FL would return Florida
+
+
+def add_state_names_column(my_df):
+    '''
+    add a column of corrsponding state names to a dataframe'
+
+    Pramas (my_df) a DataFrame with a column called "abbrev" that has state abbrevations
+
+    Return a copy of the original dataframe,but with an extra column.
+    '''
+    new_df = my_df.copy()
+
+    states = {"CA": "California", "CO": "Cplorado", "CT": "Conn"}
+
+    new_df["st_name"] = new_df["abbrev"].map(states)
+
+    breakpoint()
+
+    return my_df
+
+
+if __name__ == "__main__":
+
+    df = DataFrame({"abrev": ["CA", "CO", "CT", "DC", "TX"]})
+    print(df.head())
+
+    add_state_names_column(my_df)
+    print(df.head())
---
+++ lambdata_azico2/my_mode.py
@@ -0,0 +1,11 @@
+def enlarge(n):
+    """
+    Function will enlarge the number
+    """
+    return n * 100
+
+
+if __name__ == "___main__":
+    print('Hello')
+    y = int(input('Please choose a number'))
+    print(y,enlarge(y))
